0b919e50e91693569dd46f54fc32bf23e5e6e22a
Kevin Funk <kfunk@kde.org>
2014-07-23 10:49:44 +0200

    Copyright fixes
    Add my copyright in every file where I spent a significant amount of work

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,6 +4,7 @@
     Copyright (c) 2003 Frerich Raabe <raabe@kde.org>
     Copyright (c) 2003,2004 Stephan Kulow <coolo@kde.org>
     Copyright (c) 2003,2004 Cornelius Schumacher <schumacher@kde.org>
+    Copyright (c) 2014 Kevin Funk <kfunk@kde.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
